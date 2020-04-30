4516c36841367c6da0cd1dfdf80038da9a2bdb70
Sardar Berkal <sardarberkal@gmail.com>
2020-04-30 23:26:44 +0530

    added sysout line to ArrayListDemo program

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
+		System.out.println("Hello My ArrayList Program");
 
 		ArrayList l= new ArrayList();
 		
